42fb5658f33b7c213587fd0d1dab22d77fc7cfa7
TOUQUI <julienbeaumont@hotmail.com>
2023-09-16 15:49:30 -0400

    Déplacement a travers porte
    Ajout d'une porte qui change de scene.

---
+++ Le jeu/Carte/Porte_bleu.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var joueur_detecte = false
+
+func _ready():
+	$F.visible = false
+
+
+func _on_détecteur_joueur_body_entered(body):
+	joueur_detecte = true
+	$F.visible = true
+
+
+func _on_détecteur_joueur_body_exited(body):
+	joueur_detecte = false
+	$F.visible = false
+
+func _input(event):
+	if event.is_action_pressed("interaction") && joueur_detecte:
+		$AnimationPlayer.play("Ouverture")
+		$Timer.start()
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://Scene/SceneCorridorM.tscn")
