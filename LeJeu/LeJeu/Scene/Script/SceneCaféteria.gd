22391b86184f20cc095b18848cfc994825dde2e9
TOUQUI <julienbeaumont@hotmail.com>
2023-09-28 13:04:38 -0400

    Création chambre, caféteria
    Création des scenes et des cartes caféteria et chambre.

---
+++ LeJeu/LeJeu/Scene/Script/SceneCaféteria.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+
+func _ready():
+	pass 
+
+
+
+func _on_porte_body_entered(body):
+	pass 
---
+++ LeJeu/LeJeu/Scene/Script/scene_maison.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+func _ready():
+	pass 
+
+
+
+
+func _on_porte_body_entered(body):
+	pass
