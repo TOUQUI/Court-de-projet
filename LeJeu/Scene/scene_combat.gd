1a99474d14f10d9a28865386ac3d48ed63969a99
TOUQUI <julienbeaumont@hotmail.com>
2023-10-21 10:34:37 -0400

    Ajout de la gestion de la vie boss et joueur.

--- LeJeu/Scene/scene_combat.gd
+++ LeJeu/Scene/scene_combat.gd
@@ -4,5 +4,23 @@ extends Node2D
 signal EnleverVieJoueur(valeur:int)
 
 const ennemie = ["EnnemieUn", "EnnemieDeux"]
+var auTourDuJoueur = true
+var vieBoss = 0
 
 
+func _ready():
+	pass
+
+
+func tourJoueur():
+	$Tuile_boite_de_texte/btnFuire.enabled = true
+	$Tuile_boite_de_texte/btnAttaqueSpecial.enabled = true
+	$Tuile_boite_de_texte/btnFuire.enabled = true
+	$Tuile_boite_de_texte/btnPetiteAttaque.enabled = true
+
+
+func tourBoss():
+	$Tuile_boite_de_texte/btnFuire.enabled = false
+	$Tuile_boite_de_texte/btnAttaqueSpecial.enabled = false
+	$Tuile_boite_de_texte/btnFuire.enabled = false
+	$Tuile_boite_de_texte/btnPetiteAttaque.enabled = false
