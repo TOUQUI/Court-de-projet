46a11eb8d6b239a7564e0e81c1451d4c840a0e7c
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2023-11-07 08:33:06 -0500

    réajustement de la branche

--- Le_jeu/Le jeu/Scene/SceneQG.gd
+++
@@ -1,5 +0,0 @@
-extends StaticBody2D
-
-func _on_q_gvers_g_body_entered(body):
-	find_child("Joueur").derniere_emplacement = "QG"
-	get_tree().change_scene_to_file("res://Scene/SceneCorridorG.tscn")
